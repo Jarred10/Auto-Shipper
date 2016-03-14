783ef2759c671d97a5f73cf0abd628e3525dd93f
Jarred Green <jarredogreen@gmail.com>
2016-03-14 20:10:42 +1300

    Calendar search and listbox populated
    Created function to find SV numbers with substring and indexOf.
    
    Used this to improve earlier code about comparing SV numbers.
    
    Implemented calendar variables, finding last 2 weeks of calendar
    appointments and search the unshipped jobs to find them in calendar.

--- Auto Shipper/Form1.Designer.vb
+++ Auto Shipper/Form1.Designer.vb
@@ -23,22 +23,33 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.finderButton = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
         '
         'finderButton
         '
-        Me.finderButton.Location = New System.Drawing.Point(12, 12)
+        Me.finderButton.Location = New System.Drawing.Point(12, 417)
         Me.finderButton.Name = "finderButton"
         Me.finderButton.Size = New System.Drawing.Size(160, 31)
         Me.finderButton.TabIndex = 0
         Me.finderButton.Text = "Find Unshipped Jobs"
         Me.finderButton.UseVisualStyleBackColor = True
         '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(765, 388)
+        Me.ListBox1.TabIndex = 1
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1977, 1017)
+        Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.finderButton)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -47,4 +58,5 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents finderButton As Button
+    Friend WithEvents ListBox1 As ListBox
 End Class
--- Auto Shipper/Form1.vb
+++ Auto Shipper/Form1.vb
@@ -16,56 +16,91 @@ Public Class Form1
 
         ' Grab the sent items folder. '
         Dim myInbox As Outlook.MAPIFolder = olNs.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderSentMail)
+        Dim oCalendar As Outlook.MAPIFolder = olNs.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderCalendar)
 
         ' Set some variables. '
         Dim myItems As Outlook.Items = myInbox.Items
         Dim myItem As Object
+        Dim myCalendarItems As Outlook.Items = oCalendar.Items
+        Dim calendarItem As Object
 
         Dim partsItems As New List(Of Object)
+        Dim sentItems As New List(Of Object)
 
         ' Get a date object for two weeks ago. '
         Dim dt As Date = Date.Now.AddDays(-14)
 
         ' Limit items to last two weeks. '
         myItems = myItems.Restrict("[ReceivedTime] >= '" + dt.ToString("MM/dd/yyyy HH:mm") + "'")
+        myCalendarItems = myCalendarItems.Restrict("[Start] >= '" + dt.ToString("MM/dd/yyyy HH:mm") + "'")
+
 
         ' Sort by oldest item first. '
         myItems.Sort("[ReceivedTime]")
+        myCalendarItems.Sort("[Start]")
 
-
+        For Each calendarItem In myCalendarItems
+            Debug.WriteLine(calendarItem.Subject)
+        Next
+        ' Loops through all updates in last 2 weeks. '
         For Each myItem In myItems
-            If InStr(1, myItem.Subject, "SV") > 0 Then
-                If InStr(1, myItem.Body, "Out: ") > 0 Then
+            ' Checks if subject of update contains SV. All updates will contain this. '
+            If myItem.Subject.ToString.ToUpper.Contains("SV") Then
+                ' Checks if contents of update contain outgoing parts. '
+                If myItem.Body.ToString.ToUpper.Contains("OUT:") Then
                     Dim x As Integer
                     Dim found As Boolean = False
+                    'Checks for duplicate jobs, ie. if there is communication about a job and original update is in the email chain. '
                     For x = 1 To partsItems.Count
-                        Dim partsItem As String = partsItems.ElementAt(x - 1).Subject.ToString
-                        If partsItem.Contains(myItem.Subject.ToString) Or myItem.Subject.ToString.Contains(partsItem) Then
+                        If findSV(myItem).Contains(findSV(partsItems.ElementAt(x - 1))) Then
                             found = True
                         End If
                     Next
                     If Not found Then
+                        ' Adds mail item to a list of updates with parts. '
                         partsItems.Add(myItem)
-                        Debug.Print("Found job " + myItem.Subject + ". Received Date: " + myItem.ReceivedTime)
+                        ' TODO: remove debug statement. '
+                        Debug.Print("Found job update " + myItem.Subject + ". Received Date: " + myItem.ReceivedTime)
                     End If
+                    ' If not an update with outgoing parts, check if the job has already had its parts shipped by looking at the subject or body '
+                ElseIf myItem.Body.ToString.ToUpper.Contains("SHIPPING") Or myItem.Subject.ToString.ToUpper.Contains("SHIPPING") Then
+                    sentItems.Add(myItem)
+                    Debug.Print("Found shipped job " + myItem.Subject + ". Received Date: " + myItem.ReceivedTime)
                 End If
             End If
         Next
 
-        For Each myItem In myItems
-            If InStr(1, myItem.Subject, "SV") > 0 Then
-                If InStr(1, myItem.Body, "Shipping ") > 0 Then
-                    partsItems.Remove(myItem)
-                    Debug.Print("Removed job " + myItem.Subject)
+        ' Loops through all updates with shipped parts. '
+        For Each myItem In sentItems
+            For Each partItem In partsItems
+                If findSV(partItem).Contains(findSV(myItem)) Then
+                    partsItems.Remove(partItem)
+                    Debug.Print("Removed shipped job " + partItem.Subject)
+                    Exit For
                 End If
-            End If
+            Next
         Next
 
-        For Each myItem In partsItems
+        For Each calendarItem In myCalendarItems
+            If calendarItem.Subject.ToString.ToUpper.Contains("SV") Then
+                For Each myItem In partsItems
+                    If findSV(calendarItem).Contains(findSV(myItem)) Then
+                        ListBox1.Items.Add(calendarItem.Subject.ToString)
+                    End If
+                Next
+            End If
 
         Next
 
 
 
     End Sub
+
+    ' Function that returns the SV number of an update '
+    Public Function findSV(Update As Object) As String
+        Dim subject As String = Update.Subject.ToString.ToUpper
+        Dim jobNumberIndex = subject.IndexOf("SV")
+        Return subject.Substring(jobNumberIndex, 12)
+    End Function
+
 End Class
