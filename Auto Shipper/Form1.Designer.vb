9066ba2e4ab2f35041317f12d9808656a204f482
Jarred Green <jarredogreen@gmail.com>
2016-03-14 20:59:30 +1300

    Moved variables and parsed job.

--- Auto Shipper/Form1.Designer.vb
+++ Auto Shipper/Form1.Designer.vb
@@ -24,6 +24,10 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.finderButton = New System.Windows.Forms.Button()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'finderButton
@@ -44,19 +48,57 @@ Partial Class Form1
         Me.ListBox1.Size = New System.Drawing.Size(765, 388)
         Me.ListBox1.TabIndex = 1
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(924, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(265, 22)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(924, 49)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(265, 22)
+        Me.TextBox2.TabIndex = 3
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(924, 88)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(265, 22)
+        Me.TextBox3.TabIndex = 4
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(924, 125)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(479, 149)
+        Me.TextBox4.TabIndex = 5
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1977, 1017)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.finderButton)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents finderButton As Button
     Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
 End Class
--- Auto Shipper/Form1.vb
+++ Auto Shipper/Form1.vb
@@ -1,90 +1,75 @@
-﻿Imports Microsoft.Office
-Imports Microsoft.Office.Interop
+﻿Imports Microsoft.Office.Interop
 
 Public Class Form1
 
+    Dim olApp As Outlook.Application
+    Dim olNs As Outlook.NameSpace
+    Dim myInbox As Outlook.MAPIFolder
+    Dim myCalendar As Outlook.MAPIFolder
+
+    Dim myItems As Outlook.Items
+    Dim myCalendarItems As Outlook.Items
+
+    Dim myItem As Object
+    Dim calendarItem As Object
+
+    Dim unshippedItems As New List(Of Object)
+    Dim sentItems As New List(Of Object)
+
+    ' Get a date object for three weeks ago. '
+    Dim dt As Date = Date.Now.AddDays(-21)
+
     Private Sub finderButton_Click(sender As Object, e As EventArgs) Handles finderButton.Click
-        ' Start Outlook.
-        ' If it is already running, you'll use the same instance...
-        Dim olApp As Outlook.Application
-        olApp = CreateObject("Outlook.Application")
 
-        ' Logon. Doesn't hurt if you are already running and logged on...
-        Dim olNs As Outlook.NameSpace
-        olNs = olApp.GetNamespace("MAPI")
-        olNs.Logon()
-
-        ' Grab the sent items folder. '
-        Dim myInbox As Outlook.MAPIFolder = olNs.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderSentMail)
-        Dim oCalendar As Outlook.MAPIFolder = olNs.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderCalendar)
-
-        ' Set some variables. '
-        Dim myItems As Outlook.Items = myInbox.Items
-        Dim myItem As Object
-        Dim myCalendarItems As Outlook.Items = oCalendar.Items
-        Dim calendarItem As Object
-
-        Dim partsItems As New List(Of Object)
-        Dim sentItems As New List(Of Object)
-
-        ' Get a date object for two weeks ago. '
-        Dim dt As Date = Date.Now.AddDays(-14)
-
-        ' Limit items to last two weeks. '
+        ' Limit items to last three weeks. '
         myItems = myItems.Restrict("[ReceivedTime] >= '" + dt.ToString("MM/dd/yyyy HH:mm") + "'")
         myCalendarItems = myCalendarItems.Restrict("[Start] >= '" + dt.ToString("MM/dd/yyyy HH:mm") + "'")
 
-
-        ' Sort by oldest item first. '
+        ' Sort by oldest item first. 
         myItems.Sort("[ReceivedTime]")
         myCalendarItems.Sort("[Start]")
 
-        For Each calendarItem In myCalendarItems
-            Debug.WriteLine(calendarItem.Subject)
-        Next
-        ' Loops through all updates in last 2 weeks. '
+        ' Loops through all updates in last three weeks.
         For Each myItem In myItems
-            ' Checks if subject of update contains SV. All updates will contain this. '
+            ' Checks if subject of update contains SV. All updates will contain this.
             If myItem.Subject.ToString.ToUpper.Contains("SV") Then
-                ' Checks if contents of update contain outgoing parts. '
+                ' Checks if contents of update contain outgoing parts.
                 If myItem.Body.ToString.ToUpper.Contains("OUT:") Then
                     Dim x As Integer
                     Dim found As Boolean = False
-                    'Checks for duplicate jobs, ie. if there is communication about a job and original update is in the email chain. '
-                    For x = 1 To partsItems.Count
-                        If findSV(myItem).Contains(findSV(partsItems.ElementAt(x - 1))) Then
+                    'Checks for duplicate jobs, ie. if there is communication about a job and original update is in the email chain.
+                    For x = 1 To unshippedItems.Count
+                        If findSV(myItem.Subject.ToString).Contains(findSV(unshippedItems.ElementAt(x - 1).Subject.ToString)) Then
                             found = True
+                            Exit For
                         End If
                     Next
                     If Not found Then
-                        ' Adds mail item to a list of updates with parts. '
-                        partsItems.Add(myItem)
-                        ' TODO: remove debug statement. '
-                        Debug.Print("Found job update " + myItem.Subject + ". Received Date: " + myItem.ReceivedTime)
+                        ' Adds mail item to a list of updates with parts.
+                        unshippedItems.Add(myItem)
                     End If
-                    ' If not an update with outgoing parts, check if the job has already had its parts shipped by looking at the subject or body '
+                    ' If not an update with outgoing parts, check if the job has already had its parts shipped by looking at the subject or body.
                 ElseIf myItem.Body.ToString.ToUpper.Contains("SHIPPING") Or myItem.Subject.ToString.ToUpper.Contains("SHIPPING") Then
                     sentItems.Add(myItem)
-                    Debug.Print("Found shipped job " + myItem.Subject + ". Received Date: " + myItem.ReceivedTime)
                 End If
             End If
         Next
 
-        ' Loops through all updates with shipped parts. '
+        ' Loops through all updates with shipped parts, and removed them from unshipped parts.
         For Each myItem In sentItems
-            For Each partItem In partsItems
-                If findSV(partItem).Contains(findSV(myItem)) Then
-                    partsItems.Remove(partItem)
-                    Debug.Print("Removed shipped job " + partItem.Subject)
+            For Each partItem In unshippedItems
+                If findSV(partItem.Subject.ToString).Contains(findSV(myItem.Subject.ToString)) Then
+                    unshippedItems.Remove(partItem)
                     Exit For
                 End If
             Next
         Next
 
+        'Loops through all recent calendar items, looking for unshipped updates.
         For Each calendarItem In myCalendarItems
             If calendarItem.Subject.ToString.ToUpper.Contains("SV") Then
-                For Each myItem In partsItems
-                    If findSV(calendarItem).Contains(findSV(myItem)) Then
+                For Each myItem In unshippedItems
+                    If findSV(calendarItem.Subject.ToString).Contains(findSV(myItem.Subject.ToString)) Then
                         ListBox1.Items.Add(calendarItem.Subject.ToString)
                     End If
                 Next
@@ -96,11 +81,44 @@ Public Class Form1
 
     End Sub
 
-    ' Function that returns the SV number of an update '
-    Public Function findSV(Update As Object) As String
-        Dim subject As String = Update.Subject.ToString.ToUpper
-        Dim jobNumberIndex = subject.IndexOf("SV")
-        Return subject.Substring(jobNumberIndex, 12)
+    ' Function that returns the SV number of an update.
+    Public Function findSV(Name As String) As String
+        Return Name.Substring(Name.ToUpper.IndexOf("SV"), 12).ToUpper
     End Function
 
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        For Each myItem In unshippedItems
+            If findSV(myItem.Subject.ToString).Contains(findSV(ListBox1.SelectedItem.ToString)) Then
+                Dim contents() As String = myItem.Body.ToString.ToUpper.Split(Chr(10))
+
+                TextBox1.Text = findSV(myItem.Subject.ToString)
+
+                TextBox2.Text = contents(4)
+                TextBox3.Text = contents(5)
+                TextBox4.Text = contents(7)
+
+            End If
+        Next
+    End Sub
+
+
+    'Initialise all the needed outlook items when form is loaded.
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Start Outlook.
+        ' If it is already running, you'll use the same instance.
+        olApp = CreateObject("Outlook.Application")
+
+        ' Logon. Doesn't hurt if you are already running and logged on.
+        olNs = olApp.GetNamespace("MAPI")
+        olNs.Logon()
+
+        ' Grab the sent items folder and calendar.
+        myInbox = olNs.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderSentMail)
+        myCalendar = olNs.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderCalendar)
+
+        ' Get the list of all emails and calendar appointments.
+        myItems = myInbox.Items
+        myCalendarItems = myCalendar.Items
+
+    End Sub
 End Class
